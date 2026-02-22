5f1297b034683b715fcf929eb23120d47c0da7bf
Hleb Rubanau <g.rubanau@gmail.com>
2026-02-22 16:50:15 +0100

    wip: +debug

---
+++ debug.lua
@@ -0,0 +1,63 @@
+
+function logdebug(...)
+  if love.DEBUG then
+    local args = {...}
+    local msg = #args == 1 and args[1] or string.format(...)
+    print(msg)
+  end
+end
+
+function inspect(tname, t)
+  logdebug("TABLE: %s", tname)
+  for k,v in ipairs(t) do
+    logdebug("\t%s[%s]: %s", tname, tostring(k), tostring(v))
+  end 
+end
+
+function safe_exec(func, ...)
+    local args = {...}
+    
+    local status, result = xpcall(
+        function()
+            return func(unpack(args))
+        end,
+        function(err)
+            -- Capture full stack trace
+            local trace = debug.traceback(tostring(err), 2)
+            logdebug("Error occurred during execution:")
+            logdebug(trace)
+            return trace
+        end
+    )
+    
+    if not status then
+        error(result, 0)  -- Re-raise with preserved stack trace
+    end
+    
+    return result
+end
+
+function safe_exec_multi(func, ...)
+    local args = {...}
+    
+    local results = table.pack(xpcall(
+        function()
+            return func(unpack(args))
+        end,
+        function(err)
+            local trace = debug.traceback(tostring(err), 2)
+            logdebug("Error occurred during execution:")
+            logdebug(trace)
+            return trace
+        end
+    ))
+    
+    local status = table.remove(results, 1)
+    
+    if not status then
+        error(results[1], 0)
+    end
+    
+    return unpack(results, 1, results.n)
+end
+
